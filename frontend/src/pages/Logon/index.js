944f9fb2a89a9ac3218f471e330848edfd5f7e12
Erick <erick.munekata@gmail.com>
2020-03-29 17:37:50 -0300

    Front-end do app finalizado

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi'
 import './styles.css';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
 
 export default function Logon(){
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id });
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+            history.push('/profile');
+        } catch (error) {
+            alert('Falha no login, tente novamente');
+        }
+    }
+
     return(
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero"/>
 
-                <form>
-                <h1>Faça seu Logon</h1>
+                <form onSubmit={handleLogin}>
+                    <h1>Faça seu Logon</h1>
 
-                <input placeholder="Sua ID"/>
-                <button className="button" type="submit">Entrar</button>
+                    <input 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
+                    <button className="button" type="submit">Entrar</button>
 
-                <a href="/register">
-                    <FiLogIn size={16} color="#E02041"/>
-                    Não tem cadastro
-                </a>
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#E02041"/>
+                        Não tem cadastro
+                    </Link>
                 </form>
             </section>
 
